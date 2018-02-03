723a46fb8e8375bc065f49561bda5c999bc2534b
Yash Tewari <yashtewari1996@gmail.com>
2018-02-02 16:10:42 -0800

    Move test data into test functions.

--- match_test.go
+++ match_test.go
@@ -10,8 +10,10 @@ var (
 
 func init() {
 	initializeTestSamples()
+}
 
-	matching = map[Token][]Token{
+func TestMatching(t *testing.T) {
+	tests := map[Token][]Token{
 		testCharacters['a']: []Token{testCharacters['a'], testLowerAlphaSet, testLowerAlphaSetPlus, testDot},
 		testCharacters['P']: []Token{testUpperAlphaSetStar, testDot},
 		testDotPlus:         []Token{testDotStar, testSymbolSet, testNumSetPlus},
@@ -19,15 +21,7 @@ func init() {
 		testNumSetPlus:      []Token{testCharacters['0'], testCharacters['9'], testDotStar, NewSet([]rune{'~', 'T', '4'})},
 	}
 
-	nonMatching = map[Token][]Token{
-		testCharacters['d']: []Token{testCharacters['D'], testCharacters['b'], testNumSet},
-		testNumSetPlus:      []Token{testCharacters['.'], testCharacters['g'], testSymbolSetPlus, testLowerAlphaSet},
-		testUpperAlphaSet:   []Token{testCharacters['5'], testCharacters['j'], testSymbolSetStar, testLowerAlphaSetPlus},
-	}
-}
-
-func TestMatching(t *testing.T) {
-	for t1, t2s := range matching {
+	for t1, t2s := range tests {
 		for _, t2 := range t2s {
 			if !Match(t1, t2) {
 				t.Errorf("expected %s and %s to match, but they didn't", t1.String(), t2.String())
@@ -37,7 +31,13 @@ func TestMatching(t *testing.T) {
 }
 
 func TestNonMatching(t *testing.T) {
-	for t1, t2s := range nonMatching {
+	tests := map[Token][]Token{
+		testCharacters['d']: []Token{testCharacters['D'], testCharacters['b'], testNumSet},
+		testNumSetPlus:      []Token{testCharacters['.'], testCharacters['g'], testSymbolSetPlus, testLowerAlphaSet},
+		testUpperAlphaSet:   []Token{testCharacters['5'], testCharacters['j'], testSymbolSetStar, testLowerAlphaSetPlus},
+	}
+
+	for t1, t2s := range tests {
 		for _, t2 := range t2s {
 			if Match(t1, t2) {
 				t.Errorf("expected %s and %s not to match, but they did", t1.String(), t2.String())
